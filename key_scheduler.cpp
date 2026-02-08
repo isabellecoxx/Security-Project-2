b33b1288a935bf79aa0001753c2ca0d8de6be3d9
Clarisa-A <clarisagu45@gmail.com>
2026-02-07 20:47:18 -0600

    added starter code
    This is the starter code provided by our assignment.

---
+++ key_scheduler.cpp
@@ -0,0 +1,19 @@
+#include "key_utils.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <bitset>
+using namespace std;
+
+string shift_left(string sub_key){
+    // write code from here
+    
+    return sub_key;
+} 
+
+vector<string> key_gen(string key){
+    vector<string> sub_keys;
+    //write code from here
+    
+    return sub_keys;
+}

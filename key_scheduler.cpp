bc78808deb10eb62bf0598dd7239a15f68bee580
Isabelle Cox <isabellecox@Isabelles-MacBook-Air-2.local>
2026-02-08 13:48:28 -0600

    added shift_left function

--- key_scheduler.cpp
+++ key_scheduler.cpp
@@ -5,8 +5,24 @@
 #include <bitset>
 using namespace std;
 
+
+// function to shift the bits of the key to the left
+// used in DES key schedule to generate subkeys
+// takes a subkey as input and returns the shifted subkey
 string shift_left(string sub_key){
-    // write code from here
+     
+    // if string is empty, return it, theres nothing to shift
+    if (sub_key.empty(){
+        return sub_key;
+    })
+
+    // store the first character of the string, then shift all characters to the left
+    char first = sub_key[0];
+    for(int i = 0; i < sub_key.legth() - 1; i++){
+        sub_key[i] = sub_key[i + 1];
+    }
+    // place the first character at the end of the string
+    sub_key[sub_key.length() - 1] = first;
     
     return sub_key;
 } 
